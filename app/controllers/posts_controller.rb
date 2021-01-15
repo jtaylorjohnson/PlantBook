f87a536045be52b54706fc533aac9365a9eaef95
jtaylorjohnson <jtaylorjohnson@gmail.com>
2021-01-15 05:54:14 -0600

    posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    before_action :redirect_not_logged_in
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,16 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to root_path
     end
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect login."
+            redirect_to "/login"
+        end
+    end
+
 end
